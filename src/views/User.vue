45017c3a9d1a739c9dc0ab4b044d79d782af29c8
Tomisinteazer <tomisinteazer@gmail.com>
2025-01-20 07:38:24 +0100

    created user vue

---
+++ src/views/User.vue
@@ -0,0 +1,21 @@
+<template>
+
+    <section>
+        <h1>
+            my name is {{ name }}
+        </h1>
+    </section>
+</template>
+<script>
+
+
+export default {
+
+
+
+    props: {
+        name: String
+    }
+}
+
+</script>
