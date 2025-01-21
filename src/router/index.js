0a9d745057dbc486b4a10666bed3c86d5a93107c
Tomisinteazer <tomisinteazer@gmail.com>
2025-01-21 07:14:43 +0100

    added new route

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import About from '../views/AboutView.vue'
 import NotFound from '@/views/NotFound.vue'
 import Contact from '@/views/Contact.vue'
 import User from '@/views/User.vue'
+import HomeStore from '@/views/HomeStore.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -38,6 +39,14 @@ const router = createRouter({
       path:'/user/:name',
       name:'user',
       component:User,
+      props:true
+
+    },
+    {
+
+      path:'/store',
+      name:'homeStore',
+      component:HomeStore,
       props:true
 
     },
