9bf5eaba974c917da5245b38552fdea4546aaede
Tomisinteazer <tomisinteazer@gmail.com>
2025-01-22 04:12:47 +0100

    finished basic cart functionality

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -1,10 +1,10 @@
 import { defineStore } from "pinia";
 
-export const useCartStore = defineStore("cartStore" ,{
+export const useCartStore = defineStore("cartStore" ,
+    {
     state : ()=>(
         {
             cart:[]
-         ,
         }  
     ),
     getters:{
@@ -12,9 +12,14 @@ export const useCartStore = defineStore("cartStore" ,{
             return this.cart.reduce((a,e)=>{
                 return a + e.price
             },0)
-              
-    
-        
-            
-    }}
+    }},
+    actions:{
+        remove(i){
+            this.cart.splice(i,1)
+        },
+        addToCart(item){
+            this.cart.push(item)
+        }
+    }
+
 })
