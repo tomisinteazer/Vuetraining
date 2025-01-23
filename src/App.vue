9c219cad94449f79bf8c2842a936507c33d7b960
Tomisinteazer <tomisinteazer@gmail.com>
2025-01-23 22:27:26 +0100

    changed font ,added default data fetching, added setup function

--- src/App.vue
+++ src/App.vue
@@ -18,24 +18,65 @@
 </template>
 
 <script>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 import Navbar from './components/Navbar.vue';
+import { useUsersStore } from './stores/users';
+import { useProductsStore } from './stores/products';
+import { onBeforeMount, onMounted } from 'vue';
 
 
 export default {
 
+  setup() {
 
+
+    const productsStore = useProductsStore()
+    const usersStore = useUsersStore()
+
+
+    onBeforeMount(() => {
+      console.log("Before Mount")
+      usersStore.fetchUsers()
+      productsStore.fetchProducts()
+    })
+
+
+
+    return { productsStore, usersStore }
+
+
+
+
+
+  },
   data() {
     return {
 
-      siteName: "Sambisa",
+      siteName: "Sol✤store",
 
     }
   },
 
   components: {
     navigationbar: Navbar
+  },
+  mounted() {
+
+
   }
 }
 
 </script>
+
+
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Space+Grotesk:wght@300..700&display=swap');
+
+
+body {
+  font-family: "Space Grotesk", serif;
+  font-optical-sizing: auto;
+
+}
+</style>
