008726c879f2fe04da88a248613a371a1e3d0227
Tomisinteazer <tomisinteazer@gmail.com>
2025-02-08 23:33:52 +0100

    added single product store for fetching data

---
+++ src/stores/productStore.js
@@ -0,0 +1,30 @@
+import { defineStore } from "pinia";
+
+export let useProductsStore = defineStore("productsStore", {
+    state(){
+        return{
+            products:[],
+            loading:false,
+        }
+    },
+    actions:{
+        async fetchProducts() {
+            let finalData = [];
+            this.loading =  true
+            try {
+                const response = await fetch('https://vuetraining-products.tomisinteazer.workers.dev/')
+                if (!response.ok) {
+                    console.log('Failed to fetch users.', data);
+                }
+                finalData = await response.json();
+            } catch (error) {
+                console.error("the error is ",error.message)
+
+            } finally {
+             this.products =finalData.goods
+             this.loading = false
+         
+            }
+        }
+    }
+})
